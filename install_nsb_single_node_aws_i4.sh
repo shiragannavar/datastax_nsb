cdccc1a54dacb4c591664fd058638a7fb9dc839d
rhardaway <robert.hardaway@gmail.com>
2025-03-03 12:51:42 -0700

    nb5 download issues

--- install_nsb_single_node_aws_i4.sh
+++ install_nsb_single_node_aws_i4.sh
@@ -151,16 +151,15 @@ sudo docker run -d -v /home/ubuntu/victoria-metrics-data:/victoria-metrics-data 
 
 sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
 
-## get nosqlbench
+echo ''
+echo ''
+echo "get nosqlbench"
+
 curl -L -O https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5
 chmod u+x nb5
 
 sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y universe
 sudo DEBIAN_FRONTEND=noninteractive apt install -y libfuse2
-
-## sudo ./nb5 --list-scenarios
-## sudo ./nb5 cql_starter default.schema host=<localIP> localdc=dc1
-## sudo ./nb5 test.yaml default host=172.31.5.154 localdc=dc1 rampup-cycles=1000 main-cycles=400000
 
 echo "Lastly lets create the argsfile for the DSE scenario"
 echo "Enter the public dns name of the EC2 instance - example ec2-54-153-54-249.us-west-1.compute.amazonaws.com: "
