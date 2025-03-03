6dad7f92ee98de6315c14847f4bdf0ee5b4aef61
rhardaway <robert.hardaway@gmail.com>
2025-03-03 12:13:16 -0700

    nsb errors

--- install_nsb_single_node_aws_i4.sh
+++ install_nsb_single_node_aws_i4.sh
@@ -162,9 +162,6 @@ sudo ./nb5 --list-scenarios
 ## sudo ./nb5 cql_starter default.schema host=<localIP> localdc=dc1
 ##sudo ./nb5 test.yaml default host=172.31.5.154 localdc=dc1 rampup-cycles=1000 main-cycles=400000
 
-mkdir ./.nosqlbench
-mkdir ./.nosqlbench/grafana
-
 echo "Lastly lets create the argsfile for the DSE scenario"
 echo "Enter the public dns name of the EC2 instance - example ec2-54-153-54-249.us-west-1.compute.amazonaws.com: "
 read pub_ip
