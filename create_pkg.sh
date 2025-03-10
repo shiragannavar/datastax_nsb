486ae7414b2a144db551d642fa3bf54a92998f4a
rhardaway <robert.hardaway@gmail.com>
2025-03-10 17:14:43 -0600

    mv packaging

--- create_pkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gtar -cvf nsb_installer.tar.gz --exclude="*.DS_Store" \
-  --exclude=".git" --exclude=".gitignore" --exclude=".idea" \
-  --exclude="dashboard.json" --exclude="todos" --exclude="img"\
-  datastax_nsb
