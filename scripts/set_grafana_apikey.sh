279227492c259cba68269e0b242c32e61ad6568f
rhardaway <robert.hardaway@gmail.com>
2025-03-11 17:15:38 -0600

    last!

--- scripts/set_grafana_apikey.sh
+++ scripts/set_grafana_apikey.sh
@@ -3,6 +3,6 @@
 echo 'Log onto the Grafana UI and create an API key for the Service Account'
 echo 'then'
 echo "Enter that grafana api key you created in the UI for the Service Account:"
-read -r apikey
+read apikey
 
 echo "${apikey}" > /home/ubuntu/datastax_nsb/.nosqlbench/grafana/grafana_apikey
