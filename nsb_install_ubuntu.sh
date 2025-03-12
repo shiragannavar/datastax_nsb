84c05ee9a7cc1a9ecf9e1ab75c458d3e13779654
rhardaway <robert.hardaway@gmail.com>
2025-03-12 14:45:46 -0600

    mount issue

--- nsb_install_ubuntu.sh
+++ nsb_install_ubuntu.sh
@@ -49,13 +49,7 @@ sudo hostnamectl set-hostname "${SETHOSTNAME}"
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 
-# remount /home on nvme1n
-sudo mke2fs /dev/nvme1n1
-sudo e2label /dev/nvme1n1 home
-printf "LABEL=home\t/home\text4\tdefaults\t0 2\n" | sudo tee -a /etc/fstab
-sudo rsync -av /home/ /_home/
-sudo mount /home
-sudo rsync -av /_home/ /home/
+(. ./scripts/move_home.sh)
 
 # put /var/lib/docker on nvme2n
 sudo mke2fs /dev/nvme2n1
